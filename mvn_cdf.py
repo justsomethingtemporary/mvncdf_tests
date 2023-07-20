3814c717758063479ceac665d0c1ff59bd991c5a
justsomethingtemporary <139990474+justsomethingtemporary@users.noreply.github.com>
2023-07-20 08:28:01 -0400

    Update mvn_cdf.py

--- mvn_cdf.py
+++ mvn_cdf.py
@@ -1,15 +1,29 @@
 from scipy.stats import mvn
 import numpy as np
+import time
+from sklearn.datasets import make_spd_matrix
+import streamlit as st
 
-def mvncdf(mu, sigma, max):
-    maxar = np.full(
-        shape=np.shape(mu)[0],
-        fill_value=max
-    )
-    newloc = 2*mu - maxar
-    upp = np.full(
-        shape=np.shape(mu)[0],
-        fill_value=np.inf,
+with st.echo(code_location='below'):
+    def mvncdf(mu, sigma, max):
+        maxar = np.full(
+            shape=np.shape(mu)[0],
+            fill_value=max
         )
-    p, i = mvn.mvnun(newloc, upp, mu, sigma)
-    return p
+        newloc = 2*mu - maxar
+        upp = np.full(
+            shape=np.shape(mu)[0],
+            fill_value=np.inf,
+            )
+        p, i = mvn.mvnun(newloc, upp, mu, sigma)
+        return p
+
+    dimension = st.slider("Dimension of multivariate gaussian distribution", 1, 5000, 2000)
+    sigma = make_spd_matrix(n_dim=dimension, random_state=1)
+    max_val = st.slider("Maximum value achieved", 1, 100, 9)
+    
+    start_time = time.time()
+    p = mvncdf(np.zero(dimension), sigma, max_val)
+    st.write("Probability of a lower value is", p)
+    st.write("Time to calculate:", time.time() - start_time(), "seconds")
+    
