061de139eb1cadeddf88a036ff49499f4ff173bf
justsomethingtemporary <139990474+justsomethingtemporary@users.noreply.github.com>
2023-07-20 11:36:29 -0400

    Update mvn_cdf.py

--- mvn_cdf.py
+++ mvn_cdf.py
@@ -5,7 +5,7 @@ from sklearn.datasets import make_spd_matrix
 import streamlit as st
 
 with st.echo(code_location='below'):
-    def mvncdf(mu, sigma, max):
+    def mvncdf(mu, sigma, max, maxpts):
         maxar = np.full(
             shape=np.shape(mu)[0],
             fill_value=max
@@ -15,15 +15,16 @@ with st.echo(code_location='below'):
             shape=np.shape(mu)[0],
             fill_value=np.inf,
             )
-        p, i = mvn.mvnun(newloc, upp, mu, sigma)
+        p, i = mvn.mvnun(newloc, upp, mu, sigma, maxpts=maxpts)
         return p
 
     dimension = st.slider("Dimension of multivariate gaussian distribution", 1, 500, 5)
     sigma = make_spd_matrix(n_dim=dimension, random_state=1)
     max_val = st.slider("Maximum value achieved", -10.0, 10.0, 0.0)
+    maxpts = st.slider("Max number of points in cdf", 1000*dimension, 1000000*dimension, 1000*dimension)
     
     start_time = time.time()
-    p = mvncdf(np.zeros(dimension), sigma, max_val)
+    p = mvncdf(np.zeros(dimension), sigma, max_val, maxpts)
     st.write("Probability of a lower value is", str(p))
     s = "Time to calculate: " + str(time.time() - start_time) + " seconds"
     st.write(s)
